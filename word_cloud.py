fcdf5ea00440848e5e39858db8024adbb6eb6258
PiJ8 <pjng.dt@gmail.com>
2024-01-27 19:51:54 +0800

    Add requirements.txt for required custom lib. Add data folder with sample data for word_cloud example Fix word_cloud.py savefig result in blank image.

--- word_cloud.py
+++ word_cloud.py
@@ -1,11 +1,13 @@
+#Example of Word Cloud using data from simple txt file
+
 from matplotlib import pyplot
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 import numpy as np
 from PIL import Image
 
-### File Processing ###
+# region File Processing
 # Open and read the text file
-with open("wordcloud.txt", "r", encoding="utf-8") as file:
+with open("data\wordcloud.txt", "r", encoding="utf-8") as file:
     # Read the content of the file
     file_content = file.read()
 
@@ -14,8 +16,9 @@ word_array = file_content.split()
 
 # Display the array of words
 print(word_array)
+# endregion
 
-### Word Cloud Generation ###
+# region Word Cloud Generation
 result = word_array
 word_cloud_text = ''
 
@@ -26,19 +29,19 @@ for i in result:
 STOPWORDS.update(["see", "common"])
 
 # Image used as mask, else the wordcloud by default will be a square size
-mask = np.array(Image.open("mask_image.png"))
+mask = np.array(Image.open("data\mask_image.png"))
 color= ImageColorGenerator(mask)
 
 # Word cloud based on mask, final output will be based on image mask pixel size
-wordcloud = WordCloud(width=3800, height=3800, max_words=1000, mask=mask, stopwords=STOPWORDS, background_color="white", random_state=42).generate(wordcloudtext)
+wordcloud = WordCloud(width=3800, height=3800, max_words=1000, mask=mask, stopwords=STOPWORDS, background_color="white", random_state=42).generate(word_cloud_text)
 
 # Simple word cloud
 #wordcloud = WordCloud(background_color='white').generate(word_cloud_text)
 
 # Output the wordcloud via savefig and imshow
-# show
 pyplot.figure(figsize=(38,38))
 pyplot.imshow(wordcloud, interpolation='bilinear')
+pyplot.savefig('wordcloud.png', facecolor='k', bbox_inches='tight')
 pyplot.axis("off")
 pyplot.show()
-pyplot.savefig('wordcloud.png', facecolor='k', bbox_inches='tight')
+# endregion
